7c2498c470a84fea369ec5ae94e8cf6b30c074bc
MonacoMV <90590179+MonacoMV@users.noreply.github.com>
2021-11-22 21:09:23 -0800

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -1,49 +1,44 @@
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo "--------------------"
-echo "  Starting install  "
-echo "--------------------"
-
 timedatectl set-ntp true
 
-echo -e "-------------------------------------------------------------"
-echo -e "     **                    **        **      ** ****     ****"
-echo -e "    ****                  /**       /**     /**/**/**   **/**"
-echo -e "   **//**   ******  ***** /**       /**     /**/**//** ** /**"
-echo -e "  **  //** //**//* **///**/******   //**    ** /** //***  /**"
-echo -e " ********** /** / /**  // /**///**   //**  **  /**  //*   /**"
-echo -e "/**//////** /**   /**   **/**  /**    //****   /**   /    /**"
-echo -e "/**     /**/***   //***** /**  /**     //**    /**        /**"
-echo -e "//      // ///     /////  //   //       //     //         // "
+eche -e "   _____                .__      .____    .__                     "
+eche -e "  /  _  \_______   ____ |  |__   |    |   |__| ____  __ _____  ___"
+eche -e " /  /_\  \_  __ \_/ ___\|  |  \  |    |   |  |/    \|  |  \  \/  /"
+eche -e "/    |    \  | \/\  \___|   Y  \ |    |___|  |   |  \  |  />    < "
+eche -e "\____|__  /__|    \___  >___|  / |_______ \__|___|  /____//__/\_ \"
+eche -e "        \/            \/     \/          \/       \/            \/"
+
 
 echo "-------------------------------------------------"
 echo "-------select your disk to format----------------"
 echo "-------------------------------------------------"
-
 lsblk
 echo "Please enter disk to work on: (example /dev/sda)"
 read DISK
 echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
 read -p "are you sure you want to continue (Y/N):" formatdisk
 case $formatdisk in
-	
+
 y|Y|yes|Yes|YES)
 echo "--------------------------------------"
 echo -e "\nFormatting disk...\n$HR"
 echo "--------------------------------------"
 
-parted /dev/sda mklabel gpt mkpart P1 ext4 1MiB 8GB
-mkfs.ext4 /dev/sda1
-mount /dev/sda1 /mnt
+# create partitions
+sfdisk "${DISK}" <<EOF
+;
+EOF
 
-lsblk
+# make filesystems
+echo -e "\nCreating Filesystems...\n$HR"
+mkfs.ext4 "${DISK}1"
+mount "${DISK}1" /mnt
 
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
-
-pacstrap /mnt base base-devel linux linux-firmware vim
+pacstrap /mnt base base-devel linux linux-firmware vim --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
-genfstab -U /mnt
 
-
+echo "--------------------------------------"
+echo "--   SYSTEM READY FOR 1-setup       --"
+echo "--------------------------------------"
