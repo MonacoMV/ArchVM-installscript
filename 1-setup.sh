85ecabc553972cddabc9525c53c123d95c380b02
MonacoMV <90590179+MonacoMV@users.noreply.github.com>
2021-11-25 21:32:49 -0800

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -22,12 +22,8 @@ echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone America/Los_Angeles
-timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
-
-# Set keymaps
-localectl --no-ask-password set-keymap us
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+echo LANG=en_US.UTF-8 > /etc/locale.conf
 
 # Add sudo no password rights
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
