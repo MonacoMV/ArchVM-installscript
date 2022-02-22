988006697187b3f5b82b5dffacf817a52e817330
MonacoMV <90590179+MonacoMV@users.noreply.github.com>
2022-02-21 21:07:27 -0800

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -1,14 +1,12 @@
 echo "What do you want your username to be?"
 read USERNAME
-echo "What do you want your password to be?"
-read PASSWORD
 echo "What do you want your hostname to be?"
 read HOSTNAME
 
 # Adding user
 useradd -mg wheel "$USERNAME"
+echo "What do you want your password to be?"
 passwd "$USERNAME"
-"$PASSWORD"
 
 # Adding hostname
 echo "$HOSTNAME" >> /etc/hostname
